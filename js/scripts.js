ed0cbeb97cbd1239d49455e4a2b95baf8c76497d
WojtekKucharski <w.kucharski7@gmail.com>
2017-06-04 13:28:17 +0200

    README changed, scripts - tabs and spaces added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var a =  prompt('podaj podstawę trójkąta');
-	h =  prompt('podaj wysokość trójkąta');
-	triangleArea = a*h/2;
-alert ('Pole trójkąta o podstawie: ' + a + ' i wykości: ' + h + ' jest równe: ' + triangleArea);
+var	h =  prompt('podaj wysokość trójkąta');
+var	triangleArea = a * h / 2;
+
+alert('Pole trójkąta o podstawie: ' + a + ' i wykości: ' + h + ' jest równe: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
