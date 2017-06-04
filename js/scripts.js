e42fa9e76747e01cd99b6a29259b30af0c4a30d8
WojtekKucharski <w.kucharski7@gmail.com>
2017-06-04 23:26:17 +0200

    in README # was added, in js tabs were changed into single space

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var a =  prompt('podaj podstawę trójkąta');
-var	h =  prompt('podaj wysokość trójkąta');
-var	triangleArea = a * h / 2;
+var h =  prompt('podaj wysokość trójkąta');
+var triangleArea = a * h / 2;
 
 alert('Pole trójkąta o podstawie: ' + a + ' i wykości: ' + h + ' jest równe: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
